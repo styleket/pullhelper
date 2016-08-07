6896a543ff9842d3a9cb8fba5b3b0f4cf3c31bbf
Yusuke Shibata <shibata@fata.io>
2016-08-07 10:16:58 +0900

    desktop

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ var EventEmitter = require('event-emitter')
 var emitter = new EventEmitter()
 
 var exports = {
-	cnt:0,
+	step:0,
 	touch:false,
 	lock:false
 }
 
 var loop = (function() {
-	if(!this.touch && this.cnt > 0) {
-		this.cnt = Math.floor(this.cnt/2)
-		emitter.emit('step',this.cnt)
+	if(!this.touch && this.step > 0) {
+		this.step = Math.floor(this.step/2)
+		emitter.emit('step',this.step)
 		window.requestAnimationFrame(loop)
 	}
 }).bind(exports)
@@ -19,13 +19,13 @@ var start = (function(evt) {
 	if(this.lock) return
 	if(document.scrollingElement.scrollTop !== 0) return
 	this.touch = true
-	this.y = evt.touches[0].clientY
+	this.y = evt.touches ? evt.touches[0].clientY : evt.clientY
 }).bind(exports)
 
 var end = (function(evt) {
 	var that = this
 	if(that.lock) return
-	if(that.cnt >= 100) {
+	if(that.step >= 100) {
 		that.lock = true
 		emitter.emit('pull',function() {
 			that.lock = false
@@ -41,13 +41,13 @@ var end = (function(evt) {
 var move = (function(evt) {
 	//if(!this.touch) return
 	if(this.lock) return
-	var y = evt.touches[0].clientY
-	var cnt = Math.min(Math.max(0,this.cnt + y - this.y),100)
-	if(cnt > 0) evt.preventDefault()
-	if(cnt !== this.cnt) {
-		this.cnt = cnt
+	var y = evt.touches ? evt.touches[0].clientY : evt.clientY
+	var step = Math.min(Math.max(0,this.step + y - this.y),100)
+	if(step > 0) evt.preventDefault()
+	if(step !== this.step) {
+		this.step = step
 		this.y = y
-		emitter.emit('step',this.cnt)
+		emitter.emit('step',this.step)
 	}
 }).bind(exports)
 
@@ -59,6 +59,9 @@ exports.init = function() {
 	document.body.addEventListener('touchstart',start)
 	document.body.addEventListener('touchmove',move)
 	document.body.addEventListener('touchend',end)
+	document.body.addEventListener('mousedown',start)
+	document.body.addEventListener('mousemove',move)
+	document.body.addEventListener('mouseup',end)
 	return exports
 }
 
